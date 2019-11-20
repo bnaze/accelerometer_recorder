01fe78c3b202852997e15805bf0b88ee9116b118
baver <ec16389@qmul.ac.uk>
2019-11-20 13:10:04 +0000

    calibration + filter

--- app/src/main/java/com/example/projectapplication/Graph.java
+++ app/src/main/java/com/example/projectapplication/Graph.java
@@ -10,7 +10,10 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Vibrator;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -18,22 +21,14 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Graph extends AppCompatActivity implements SensorEventListener {
 
     private GraphView graph;
     private LineGraphSeries<DataPoint> series;
     private SensorManager sensorManager;
     private Sensor sensor;
-    private int time;
 
-    private boolean checkSensor = true;
-    private Timer timer;
-    private TimerTask timerTask;
-    private Date date;
+    private boolean record = false;
 
     private String fileRoot;
     private File dataFileDir;
@@ -44,6 +39,19 @@ public class Graph extends AppCompatActivity implements SensorEventListener {
     private float dy = 0;
     private float dz = 0;
 
+    private float lastX;
+    private float lastY;
+    private float lastZ;
+
+    private TextView currentX;
+    private TextView currentY;
+    private TextView currentZ;
+
+    private float vibrateThreshold;
+    private Vibrator vibrator;
+
+    private Button recordButton;
+    private Button deleteButton;
 
     private String fileName = "datasample.txt";
 
@@ -52,76 +60,119 @@ public class Graph extends AppCompatActivity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
+        //Assign file variables used to create files for analysis
         fileRoot = Environment.getExternalStorageDirectory().toString();
         dataFileDir = new File(fileRoot);
         dataFile = new File(dataFileDir, fileName);
 
-        date = new Date();
-        timer = new Timer();
-        timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                checkSensor = true;
-            }
-        };
-
-        //Change time to get sensor readings
-//        timer.schedule(timerTask,0,0);
-
-        graph = findViewById(R.id.graph);
-        series = new LineGraphSeries<>();
-        graph.addSeries(series);
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(40);
-
-
+        //Create sensors here
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //initialise views
+        currentX = findViewById(R.id.currentX);
+        currentY = findViewById(R.id.currentY);
+        currentZ = findViewById(R.id.currentZ);
+
+        vibrateThreshold = sensor.getMaximumRange() / 2;
+        vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+
+        recordButton = findViewById(R.id.record);
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(record == false) {
+                    recordButton.setText("Stop Recording");
+                    record = true;
+                }
+                else{
+                    recordButton.setText(("Record"));
+                    record = false;
+                }
+            }
+        });
+
+        deleteButton = findViewById(R.id.deleteExisting);
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(dataFile.exists()){
+                    dataFile.delete();
+                    dataFile = new File(dataFileDir, fileName);
+                }
+            }
+        });
+    }
+
+    public void resetDisplay(){
+        currentX.setText("0.0");
+        currentY.setText("0.0");
+        currentZ.setText("0.0");
+    }
+
+    public void displayReadings(){
+        currentX.setText(Float.toString(dx));
+        currentY.setText(Float.toString(dy));
+        currentZ.setText(Float.toString(dz));
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (checkSensor) {
-            //checkSensor = false;
-            time++;
+        resetDisplay();
 
-            if(event.values[0] < 0){
-                dx = event.values[0];
-            }
-            else if(event.values[1] < 0){
-                dy = event.values[1];
-            }
-            else if(event.values[2] < 0){
-                dz = event.values[2];
-            }
+        displayReadings();
 
-            try {
-                DataPoint newData = new DataPoint(time, event.values[0]+dx);
-                series.appendData(newData, true, 40);
-            } catch (Exception e) {
-                Log.d("Exception1", e.getMessage());
-            }
+        dx = Math.abs(lastX - event.values[0]);
+        dy = Math.abs(lastY - event.values[1]);
+        dz = Math.abs(lastZ - event.values[2]);
 
-            //FileOutputStream fos = null;
-            String entry = event.values[0] + "," + event.values[1] + "," + event.values[2] + "," + System.currentTimeMillis();
+        /*
+        if(event.values[0] < 0){
+            dx = event.values[0];
+        }
+        else if(event.values[1] < 0){
+            dy = event.values[1];
+        }
+        else if(event.values[2] < 0){
+            dz = event.values[2];
+        }
+         */
 
+        if (dx < 2)
+            dx = 0;
+        if (dy < 2)
+            dy = 0;
+        if (dz < 2){
+            dz = 0;
+        }
+    if (dz > vibrateThreshold || (dy > vibrateThreshold) || (dx > vibrateThreshold)) {
+            vibrator.vibrate(50);
+        }
+
+        String entry = event.values[0] + "," + event.values[1] + "," + System.currentTimeMillis();
+
+        if(record) {
             try {
                 writer = new BufferedWriter(new FileWriter(dataFile, true));
                 writer.write(entry);
                 writer.newLine();
                 writer.close();
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
+        vibrate();
     }
 
-
-
+    public void vibrate() {
+        if ((dx > vibrateThreshold) || (dy > vibrateThreshold) || (dz > vibrateThreshold)) {
+            vibrator.vibrate(50);
+        }
+    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
